80f4b16a14a9e367213b54b0d6d7357815f8bff6
DWAA1660 <dwatnip123@gmail.com>
2024-01-04 10:41:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const wss = new WebSocket.Server({ noServer: true });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  console.log(wss.clients)
 
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
     // Send the received message back to the client
     console.log(message + "h")
+    
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(`Server: ${message}`);
